f76c4d811b1c1c088ad140f38e1b25458ccd258a
mathewsboscolo <mathewsboscolo@outlook.com>
2023-04-10 20:16:10 -0300

    general update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,40 +5,13 @@ const app = admin.initializeApp();
 const db = app.firestore();
 const dentistCol = db.collection("Dentists");
 
-export const addSampleDentist = functions
-  .region("southamerica-east1")
-  .https.onRequest(async (request, response) => {
-    const dentist = {
-      nome: "Lucas Perin Malvestiti",
-      telefone: "(19)999999999",
-      email: "emaillucas@email.com",
-    };
-    try {
-      const docRef = await dentistCol.add(dentist);
-      response.send("Sample Dentist has been succefulyadded. Reference:" +
-      docRef.id);
-    } catch (e) {
-      functions.logger.error("Error while adding Sample Dentist");
-      response.send("Error while adding Sample Dentist");
-    }
-  });
-
-export const deleteSampleDentist = functions
-  .region("southamerica-east1")
-  .https.onRequest(async (request, response) => {
-    const dentistId = "wo0j54jnrEcLQ98FEJqR";
-    await dentistCol.doc(dentistId).delete();
-    response.send("Deleting dentist probably done");
-  });
-
-export const showSampleDentistsLucas = functions
-  .region("southamerica-east1")
-  .https.onRequest(async (request, response) => {
-    const Dentists : FirebaseFirestore.DocumentData = [];
-    const snapshot = await dentistCol.where("nome", "==", "Lucas").get();
-    snapshot.forEach((doc) => {
-      Dentists.push(doc.data());
-    });
-    response.status(200).json(Dentists);
-  });
-  
+export const salvarDadosDentista = functions.
+    region("southamerica-east1")
+    .https.onCall(async (data) => {
+      try {
+        await dentistCol.doc().set(data)
+        return { text: "Inserido" }
+      } catch (error) { 
+        return {text: "Erro ao inserir."}
+      }
+});
